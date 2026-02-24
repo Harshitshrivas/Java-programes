9eb17af7196ee69ddc6f061a7d9ca28a91a1f3d4
Harshit Shrivas <harshitshrivas88@gmail.com>
2026-02-24 14:29:58 +0530

    insert k element

--- LinkedList/insert.java
+++ LinkedList/insert.java
@@ -39,6 +39,37 @@ public class insert {
         currNode.next = newNode;
     }
 
+    public void addAtindex(int index, int data) {
+
+        if (index < 0) {
+            System.out.println("Invalid index");
+            return;
+        }
+
+        Node newNode = new Node(data);
+        if (index == 0) {
+            newNode.next = head;
+            head = newNode;
+            return;
+        }
+
+        Node currNode = head;
+        int count = 0;
+
+        while (currNode != null && count < index - 1) {
+            currNode = currNode.next;
+            count++;
+        }
+
+        if (currNode == null) {
+            System.out.println("Index out of bounds");
+            return;
+        }
+        newNode.next = currNode.next;
+        currNode.next = newNode;
+
+    }
+
     public void printList() {
         Node currNode = head;
         while (currNode != null) {
@@ -59,6 +90,14 @@ public class insert {
         list.addfirst(1);
 
         System.out.println("Linked List:");
+        list.printList();
+
+        System.out.println("After adding 15 at index 3:");
+        list.addAtindex(3, 15);
+        list.printList();
+
+        System.out.println("After adding 25 at index 5:");
+        list.addAtindex(5, 25);
         list.printList();
     }
 }
