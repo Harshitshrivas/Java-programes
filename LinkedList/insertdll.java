5b4f413ceaff600958e05f16c071ca7df0a506bc
Harshit Shrivas <harshitshrivas88@gmail.com>
2026-02-25 14:57:27 +0530

    Insert at k position

--- LinkedList/insertdll.java
+++ LinkedList/insertdll.java
@@ -45,6 +45,43 @@ public class insertdll {
         newNode.prev = currNode;
     }
 
+    public void insertatindex(int index, int data) { // method to insert a node at a specific index in the doubly linked list
+
+        if (index < 0) {
+            System.out.println("Invalid index");
+            return;
+        }
+
+        Node newNode = new Node(data);
+
+        if (index == 0) {
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+            return;
+        }
+        Node currNode = head;
+        int count = 0;
+
+        while (currNode != null && count < index - 1) {
+            count++;
+            currNode = currNode.next;
+        }
+
+        if (currNode == null) {
+            System.out.println("Index out of bounds");
+            return;
+        }
+        newNode.next = currNode.next; // update the next pointer of the new node to point to the next node of the current node
+        newNode.prev = currNode;
+
+        if (currNode.next != null) {  // check if the next node is not null before updating its prev pointer
+            currNode.next.prev = newNode;
+        }
+        currNode.next = newNode;
+
+    }
+
     public void printlist() {
 
         Node currNode = head;
@@ -52,6 +89,19 @@ public class insertdll {
         while (currNode != null) {
             System.out.print(currNode.data + " <-> ");
             currNode = currNode.next;
+        }
+        System.out.println("null");
+    }
+    public void printback() {
+        Node cuNode = head;
+
+        while (cuNode.next != null) { // Move to last node
+            cuNode = cuNode.next;
+        }
+
+        while (cuNode != null) { // Traverse backword
+            System.out.print(cuNode.data + " -> ");
+            cuNode = cuNode.prev;
         }
         System.out.println("null");
     }
@@ -72,6 +122,13 @@ public class insertdll {
         list.insertatend(12);
         System.out.println("Doubly Linked List after inserting at the end:");
         list.printlist();
+
+        list.insertatindex(2, 5);
+        System.out.println("Doubly Linked List after inserting at index 2:");
+        list.printlist();
+
+        System.out.println("Doubly Linked List printed in reverse:");
+        list.printback();
 
     }
 }
