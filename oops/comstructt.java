50bb0c59ea22db6850f02d2ede17072931b49df5
Harshit Shrivas <harshitshrivas88@gmail.com>
2026-03-16 14:58:25 +0530

    Constructor

--- oops/comstructt.java
+++ oops/comstructt.java
@@ -1,19 +1,54 @@
 
+// class Student {
+
+//     int age;
+//     String name;
+
+//     Student() {
+//         System.out.println("Default Constuctor");
+//     }
+
+//     Student(int a, String s) {  // paramaterised constuctor
+//         age = a;
+//         name = s;
+//     }
+
+//     void display() {
+//         System.out.println(name + " " + age);
+//     }
+
+// }
+
 class Student {
 
-    int age;
     String name;
+    int age;
+
+    static int count = 0;
+
+    static { // ya Constructor ka pehla chalego
+        System.out.println("Student class loaded");
+    }
 
     Student() {
-        System.out.println("Default Constuctor");
+        name = "Rajuu";
+        age = 21;
+        count++;
     }
 
-    Student(int a, String s) {  // paramaterised constuctor
-        age = a;
-        name = s;
+    Student(String n, int a) {
+        this.name = n;
+        this.age = a;
+        count++;
     }
 
-    void display() {
+    Student(String name) {
+        this.name = name;
+        age = 22;
+        count++;
+    }
+
+    void dislplay() {
         System.out.println(name + " " + age);
     }
 
@@ -22,7 +57,13 @@ class Student {
 public class comstructt {
     public static void main(String[] args) {
         Student s1 = new Student();
-        Student s2 = new Student(21, "Harshit");
-        s2.display();
+        Student s2 = new Student("Kamal Hasan");
+        Student S3 = new Student("Harshit", 21);
+
+        s1.dislplay();
+        s2.dislplay();
+        S3.dislplay();
+
+        System.out.println("Total Student: " + Student.count);
     }
 }
