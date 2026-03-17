914f23f1fdd899a0e7a213fcf0ef5bc75e10fd44
Harshit Shrivas <harshitshrivas88@gmail.com>
2026-03-17 23:35:15 +0530

    Collection framework

---
+++ Stack/frameworkk.java
@@ -0,0 +1,25 @@
+
+import java.util.Stack;
+
+public class frameworkk {
+    public static void main(String[] args) {
+        Stack<Integer> st = new Stack<>();
+
+        st.push(10);
+        st.push(20);
+        st.push(30);
+        st.push(40);
+        st.push(50);
+        System.out.println("Stack: " + st);
+
+        st.pop();
+        System.out.println("After removing last element: " + st);
+        System.out.println("The top of stack is " + st.peek());
+
+        System.out.println("Reverse of Stack is: ");
+        while(!st.isEmpty()){
+            System.out.print(st.peek() + " ");
+            st.pop();
+        }
+    }
+}
