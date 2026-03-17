54c63d0dfab9abf94e13fe79465418752e01cf46
Harshit Shrivas <harshitshrivas88@gmail.com>
2026-03-18 00:33:07 +0530

    Collection

--- Stack/frameworkk.java
+++ Stack/frameworkk.java
@@ -15,11 +15,18 @@ public class frameworkk {
         st.pop();
         System.out.println("After removing last element: " + st);
         System.out.println("The top of stack is " + st.peek());
+        System.out.println("Is Empty: " + st.empty());
+        System.out.println("The postion of stack 10 is: " + st.search(10));
+        System.out.println("The Size of stack is: " + st.size());
+        System.out.println("Stack contaion 20: " + st.contains(20));
+        System.out.println("First element: " + st.firstElement());
+        System.out.println("Last element: " + st.lastElement());
 
         System.out.println("Reverse of Stack is: ");
-        while(!st.isEmpty()){
+        while (!st.isEmpty()) {
             System.out.print(st.peek() + " ");
             st.pop();
         }
+        System.out.println();
     }
 }
