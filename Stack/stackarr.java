53bbdd55dae1644b1a5efec52d97416cc1a2803a
Harshit Shrivas <harshitshrivas88@gmail.com>
2026-03-18 23:57:22 +0530

    Stack array

---
+++ Stack/stackarr.java
@@ -0,0 +1,74 @@
+public class stackarr {
+
+    static int max = 5;
+    static int[] stack = new int[max];
+    static int top = -1;
+
+    static void push(int value) {
+        if (top == max - 1) {
+            System.out.println("Stack Overflow..");
+        } else {
+            top++;
+            stack[top] = value;
+        }
+    }
+
+    static void pop() {
+        if (top == -1) {
+            System.out.println("Stack Empty..");
+        } else {
+            System.out.println("Deleted: " + stack[top]);
+            top--;
+        }
+    }
+
+    static void peek() {
+        if (top == -1) {
+            System.out.println("Stack Empty..");
+        } else {
+            System.out.println("Top: " + stack[top]);
+        }
+    }
+
+    static boolean isEmpty() {
+        return top == -1;
+    }
+
+    static int size() {
+        return top + 1;
+    }
+
+    static boolean isFull() {
+        return top == max - 1;
+    }
+
+    static void displayyy() {
+        if (top == -1) {
+            System.out.println("Stack Empty..");
+            return;
+        }
+        System.out.print("Stack elements: ");
+        for (int i = top; i >= 0; i--) {
+            System.out.print(stack[i] + " ");
+        }
+        System.out.println();
+
+    }
+
+    public static void main(String[] args) {
+        push(10);
+        push(20);
+        push(30);
+        push(40);
+        displayyy();
+        pop();
+        displayyy();
+        peek();
+        System.out.println("Is Empty: " + isEmpty());
+        System.out.println("Size: " + size());
+        System.out.println("Is Full: " + isFull());
+        pop();
+        displayyy();
+
+    }
+}
