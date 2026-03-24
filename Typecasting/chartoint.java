c2d3c4cf9f100fc83200633d84db7fb0fd169281
Harshit Shrivas <harshitshrivas88@gmail.com>
2026-03-24 21:33:03 +0530

    Chat to int , int to char

---
+++ Typecasting/chartoint.java
@@ -0,0 +1,16 @@
+public class chartoint {
+    public static void main(String[] args) {
+        char ch = 'A';
+        int aa = ch;
+
+        System.out.println("Character: " + ch);
+        System.out.println("Integer: " + aa);
+
+        int x = 66;
+        char y = (char) x;
+
+        System.out.println("Integer: " + x);
+        System.out.println("Character: " + y);
+
+    }
+}
