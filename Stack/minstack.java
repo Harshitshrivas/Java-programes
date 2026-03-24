c870f5830d61ca2a90b3fe6dcef01e1f246de722
Harshit Shrivas <harshitshrivas88@gmail.com>
2026-03-24 13:37:20 +0530

    Min Stack pair approch

--- Stack/minstack.java
+++ Stack/minstack.java
@@ -1,6 +1,59 @@
+
+import java.util.Stack;
+
+class Pair {
+
+   int val;
+   int min;
+
+   public Pair(int val, int min) {
+
+      this.val = val;
+      this.min = min;
+   }
+}
+
 public class minstack {
+
+   Stack<Pair> st = new Stack<>();
+
+   public void push(int x) {
+
+      if (st.isEmpty()) {
+         st.push(new Pair(x, x));
+      } else {
+         int currentmin = Math.min(x, st.peek().min);
+         st.push(new Pair(x, currentmin));
+      }
+   }
+
+   public void pop() {
+      if (!st.isEmpty()) {
+         st.pop();
+      }
+   }
+
+   public int top() {
+      return st.peek().val;
+   }
+
+   public int getmin() {
+      return st.peek().min;
+   }
+
    public static void main(String[] args) {
-       
-   
-   } 
+
+      minstack ms = new minstack();
+      ms.push(10);
+      ms.push(40);
+      ms.push(8);
+      ms.push(50);
+      ms.push(5);
+
+      System.out.println( "Minimum in stack are: " + ms.getmin());
+      System.out.println("Top of stack are: " + ms.top());
+      ms.pop();
+      System.out.println( "Minimum in stack are: " + ms.getmin());
+      System.out.println("Top of stack are: " + ms.top());
+   }
 }
