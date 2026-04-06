a28fc31303bab7cdc72d0589baf0af781603b508
Harshit Shrivas <harshitshrivas88@gmail.com>
2026-04-06 21:27:21 +0530

    Hashmapping

---
+++ mapp.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+
+public class mapp {
+    public static void main(String[] args) {
+        HashMap<Integer, String> map = new HashMap<>();
+
+        map.put(1, "Harshit");
+        map.put(2, "Harshu");
+        map.put(3, "Harshit Shrivas");
+        map.put(4, "Harshvardhan");
+        map.put(5, "Harsh Rai");
+
+        System.out.println(map);
+    }
+}
