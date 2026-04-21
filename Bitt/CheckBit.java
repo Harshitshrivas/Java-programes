4ca8e28d72cb6665916f05e9a3d1ab320b9d81d8
Harshit Shrivas <harshitshrivas88@gmail.com>
2026-04-21 11:19:12 +0530

    Checking  if i-th bit is set or not

--- Bitt/CheckBit.java
+++ Bitt/CheckBit.java
@@ -3,7 +3,8 @@ public class CheckBit {
         int num = 9; // Number to check
         int position = 3; // Position to check (0-based index)
 
-        if((num & (1 << position)) != 0){
+        // << is the left shift operator, it shifts the bits of 1 to the left by 'position' places like 1 << 3 will give 8 (1000 in binary)
+        if((num & (1 << position)) != 0){ // Check if the bit at the specified position is set
             System.out.println("Bit at position " + position + " is set (1)");
         }else{
             System.out.println("Bit at position " + position + " is not set (0)");
