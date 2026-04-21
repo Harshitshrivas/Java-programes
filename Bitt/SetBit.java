73f7a25e85025f6fdc00094d785a136368420b6b
Harshit Shrivas <harshitshrivas88@gmail.com>
2026-04-21 13:59:20 +0530

    Set the i-th bit

---
+++ Bitt/SetBit.java
@@ -0,0 +1,11 @@
+public class SetBit {
+    public static void main(String[] args) {
+        int num =9;
+        int position = 2; // Position to set (0-based index)
+
+         // << is the left shift operator, it shifts the bits of 1 to the left by 'position' places like 1 << 3 will give 8 (1000 in binary)
+        num = num | (1 << position); // Set the bit at the specified position
+        System.out.println("Number after setting bit at position " + position + ": " + num);
+        
+    }
+}
