8007c8c2133f66509f7dd23ba78fe12e419108ae
Harshit Shrivas <harshitshrivas88@gmail.com>
2026-04-22 21:28:17 +0530

    Check if a number is power of 2 or not

--- Bitt/checkpower2.java
+++ Bitt/checkpower2.java
@@ -13,5 +13,8 @@ public class checkpower2 {
             System.out.println(num + " is not a power of 2.");
         }
         sc.close();
+        // A number that is a power of 2 has only one set bit in its binary representation. For example, 1 (2^0), 2 (2^1), 4 (2^2), 8 (2^3), etc.
+        // The expression (num & (num - 1)) checks if there is only one set bit in num. If num is a power of 2, then num - 1 will have all bits set to the right of the single set bit in num, and the bitwise AND will result in 0.
+        // For example, if num is 4 (100 in binary), then num - 1 is 3 (011 in binary). The bitwise AND of 4 and 3 is 0 (100 & 011 = 000), which confirms that 4 is a power of 2. If num is not a power of 2, the result will be non-zero.
     }
 }
