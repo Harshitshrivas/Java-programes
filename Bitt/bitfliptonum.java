c769e1ee8bd750e1dea8cfd546d2b00f3438a983
Harshit Shrivas <harshitshrivas88@gmail.com>
2026-04-25 14:39:37 +0530

    Minimum Bit Flips to Convert Number

---
+++ Bitt/bitfliptonum.java
@@ -0,0 +1,14 @@
+public class bitfliptonum {
+    public static void main(String[] args) {
+        int start = 10;
+        int goal = 7;
+        int count = 0;
+        int xor = start ^ goal;
+
+        while (xor > 0) {
+            count += xor & 1;
+            xor >>= 1;
+        }
+        System.out.println(" " + count);
+    }
+}
