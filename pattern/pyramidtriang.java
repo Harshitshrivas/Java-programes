e70c01846daffe2de7756dfe1bf3ab476ae2181a
Harshit Shrivas <harshitshrivas88@gmail.com>
2026-05-01 13:08:14 +0530

    Pyramid

--- pattern/pyramidtriang.java
+++ pattern/pyramidtriang.java
@@ -1,7 +1,8 @@
 public class pyramidtriang {
     public static void main(String[] args) {
         int row = 6;
-
+          
+        System.out.println("Pyarmid: ");
         for (int i = 1; i <= row; i++) {
             for (int j = 1; j <= row - i; j++) {
                 System.out.print(" ");
@@ -11,5 +12,17 @@ public class pyramidtriang {
             }
             System.out.println();
         }
+
+        System.out.println("Inverted Pyramid:  ");
+         for (int i = row; i >=1; i--) {
+            for (int j = 1; j <= row - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
     }
 }
