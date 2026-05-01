fe373537a547bd11266125efd86f7e6b1a8cba5e
Harshit Shrivas <harshitshrivas88@gmail.com>
2026-05-01 12:18:38 +0530

    Rectangle patern

---
+++ pattern/rectangle.java
@@ -0,0 +1,14 @@
+
+public class rectangle {
+    public static void main(String[] args) {
+        int row = 4;
+        int coloumn = 5;
+
+        for(int i =1; i<=row; i++){
+            for(int j = 1;j<=coloumn; j++){
+                System.out.print("* ");
+            }
+            System.out.println(); // Move to the next line after each row
+        }
+    }
+}
