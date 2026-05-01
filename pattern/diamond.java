8db2ae18f2564d89a7ab03e35a5eb2772e799d7e
Harshit Shrivas <harshitshrivas88@gmail.com>
2026-05-01 13:46:23 +0530

    Diamond Problem

---
+++ pattern/diamond.java
@@ -0,0 +1,27 @@
+public class diamond {
+    public static void main(String[] args) {
+        int row = 6;
+        // Upper Pyramid
+        for (int i = 1; i <= row; i++) {
+            for (int j = 1; j <= row - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // Lower Pyramid
+
+        for (int i = row - 1; i >= 1; i--) {
+            for (int j = 1; j <= row - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
