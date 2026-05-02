598efa8307b8e7fa1355f88c5f5191a98ee7428f
Harshit Shrivas <harshitshrivas88@gmail.com>
2026-05-02 23:31:45 +0530

    Hollow Diamond

---
+++ pattern/hollowdiamond.java
@@ -0,0 +1,35 @@
+public class hollowdiamond {
+    public static void main(String[] args) {
+        int row = 6;
+        // Upper Pyramid
+        for (int i = 1; i <= row; i++) {
+            for (int j = 1; j <= row - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                if (k == 1 || k == (2 * i - 1)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+        // Lower Pyramid
+
+        for (int i = row - 1; i >= 1; i--) {
+            for (int j = 1; j <= row - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                if (k == 1 || k == (2 * i - 1)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
