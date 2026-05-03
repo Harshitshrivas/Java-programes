31d026443e097bb303045a2e7440d86f985f9378
Harshit Shrivas <harshitshrivas88@gmail.com>
2026-05-03 14:34:54 +0530

    Priority queue- Max heap default

---
+++ heap/baseeic.java
@@ -0,0 +1,16 @@
+
+import java.util.PriorityQueue;
+
+public class baseeic {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(); // Min-heap by default
+        pq.add(10);
+        pq.add(20);
+        pq.add(5);
+        pq.add(15);
+        System.out.println("Priority Queue: " + pq); // Note: The internal order may not be sorted
+        System.out.println("Peek: " + pq.peek());
+        System.out.println("Poll: " + pq.poll());
+        System.out.println("Priority Queue after poll: " + pq);
+    }
+}
