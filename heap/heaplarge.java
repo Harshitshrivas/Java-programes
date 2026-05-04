bc2e0887c00b6f87c5bf3e369244e913067c1888
Harshit Shrivas <harshitshrivas88@gmail.com>
2026-05-04 13:19:09 +0530

    K Large and all K large elemnet

--- heap/heaplarge.java
+++ heap/heaplarge.java
@@ -1,11 +1,11 @@
 
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class heaplarge {
     public static void main(String[] args) {
 
         int arr[] = { 10, 5, 21, 30, 7, 45, 2, 11 };
-        int k = 2; // Change this value to find the k-th largest element
+        int k = 3; // Change this value to find the k-th largest element
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         for (int i = 0; i < arr.length; i++) {
             pq.add(arr[i]);
@@ -14,5 +14,11 @@ public class heaplarge {
             }
         }
         System.out.println("The " + k + "-th largest element is: " + pq.peek());
+
+
+        // K Largest Elements (Return all k elements)
+        List<Integer> res = new ArrayList<>(pq);
+        //Collections.sort(res); 
+        System.out.println(res);
     }
 }
