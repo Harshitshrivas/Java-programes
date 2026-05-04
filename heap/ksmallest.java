4ead89feb99a540e3ee3e123aa0dae6eb8140856
Harshit Shrivas <harshitshrivas88@gmail.com>
2026-05-04 13:21:11 +0530

    K Smallest and All k samlll

--- heap/ksmallest.java
+++ heap/ksmallest.java
@@ -1,11 +1,13 @@
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.PriorityQueue;
 
 public class ksmallest {
     public static void main(String[] args) {
         int arr[] = { 10, 5, 21, 30, 7, 45, 2, 11 };
-        int k = 2; // Change this value to find the k-th smallest element
+        int k = 4; // Change this value to find the k-th smallest element
         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder()); // Max-heap
         for (int i = 0; i < arr.length; i++) {
             pq.add(arr[i]);
@@ -14,5 +16,9 @@ public class ksmallest {
             }
         }
         System.out.println("The " + k + "-th smallest element is: " + pq.peek());
+
+        // All K sa chotee
+        List<Integer> res =new ArrayList<>(pq);
+        System.out.println("All Smallest K element are: "+ res);
     }
 }
