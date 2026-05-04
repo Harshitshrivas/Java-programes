bfd1b9788b267d6b12f3b2a311718dd85c880443
Harshit Shrivas <harshitshrivas88@gmail.com>
2026-05-04 09:37:16 +0530

    K-th Smallest Element (Max Heap)

---
+++ heap/ksmallest.java
@@ -0,0 +1,18 @@
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class ksmallest {
+    public static void main(String[] args) {
+        int arr[] = { 10, 5, 21, 30, 7, 45, 2, 11 };
+        int k = 2; // Change this value to find the k-th smallest element
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder()); // Max-heap
+        for (int i = 0; i < arr.length; i++) {
+            pq.add(arr[i]);
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
+        System.out.println("The " + k + "-th smallest element is: " + pq.peek());
+    }
+}
