3f1a31c05c28a029348527e05e32274b2c995b8d
Harshit Shrivas <harshitshrivas88@gmail.com>
2026-05-13 21:32:41 +0530

    Boats to Save People

---
+++ Greedy/bootsave.java
@@ -0,0 +1,24 @@
+
+import java.util.Arrays;
+
+public class bootsave {
+    public static void main(String[] args) {
+        int [] peeple = {3, 2, 2, 1};
+        int limit = 3;
+        int count = 0;
+        Arrays.sort(peeple);
+        int i = 0, j = peeple.length-1;
+
+        while( i<= j){
+            if(peeple[i] + peeple[j] <= limit){
+                i ++;
+                j--;
+                
+            }else{
+                j--;
+            }
+            count++;
+        }
+        System.out.println("count of boats needed: " + count);
+    }
+}
