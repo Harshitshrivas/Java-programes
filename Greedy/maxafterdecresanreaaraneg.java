8d9f843625d01bc2ae5319ef4c9f589d97a5316d
Harshit Shrivas <harshitshrivas88@gmail.com>
2026-05-15 11:21:37 +0530

    Maximum Element After Decreasing and Rearranging

--- Greedy/maxafterdecresanreaaraneg.java
+++ Greedy/maxafterdecresanreaaraneg.java
@@ -5,24 +5,19 @@ import java.util.Arrays;
 public class maxafterdecresanreaaraneg {
 
     public static void main(String[] args) {
-       // int arr[] = {2 , 2, 1, 2, 1 };
-         int arr[] = {100 ,  1 , 1000};
+        int arr[] = {2 , 2, 1, 2, 1 };
+       //  int arr[] = {100 ,  1 , 1000};
         int n = arr.length;
-    
-
+        int max = Integer.MIN_VALUE;
+        arr[0] = 1;
         Arrays.sort(arr);
         // Maximum Element After Decreasing and Rearranging
-        for(int i =0; i<n; i++){
-            if(i == 0){
-                arr[i] = 1;
-            }else{
-                if(arr[i] - arr[i -1] > 1){
-                    arr[i] = arr[i-1] + 1;
-                }else{
-                    arr[i] = arr[i-1];
-                }
+        for(int i =1; i<n; i++){
+            if(arr[i] - arr[i-1] > 1){
+                arr[i] = arr[i-1] + 1;
             }
+            max = Math.max(max, arr[i]);
         }
-        System.out.println(arr[n-1]);
+        System.out.println(max);
     }
 }
