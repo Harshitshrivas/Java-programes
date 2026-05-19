9340aae74c2ac014b0b4f687a8d602ba2fd21e2c
Harshit Shrivas <harshitshrivas88@gmail.com>
2026-05-19 11:34:59 +0530

    Fixed Size Window

--- SlidingWindow/fixedsizewindow.java
+++ SlidingWindow/fixedsizewindow.java
@@ -1,15 +1,19 @@
 public class fixedsizewindow {
     public static void main(String[] args) {
 
-        int [] nums = {2,1,5,1,3,2};
+        int[] nums = { 2, 1, 5, 1, 3, 2 };
         int k = 3;
 
-        for(int i =0; i<nums.length - k +1; i++){
-            int sum = 0;
-            for(int j = i; j < i + k; j++){
-                sum += nums[j];
-            }
-            System.out.println("Sum of subarray starting at index " + i + ": " + sum);  
-        }  
+        int sum = 0;
+        for (int i = 0; i < k; i++) { // calculate the sum of the first window
+            sum += nums[i];
+        }
+        int maxSum = sum;
+        for (int j = k; j < nums.length; j++) {
+            sum += nums[j] - nums[j - k]; // add the next element and remove the first element of the previous window
+            maxSum = Math.max(maxSum, sum); // update the maximum sum if the
+        }
+        System.out.println("The maximum sum of a subarray of size " + k + " is: " + maxSum);
+
     }
 }
