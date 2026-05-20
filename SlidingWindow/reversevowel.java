910e33fea61fa2f9d9ab3d81c59692b88c89cb52
Harshit Shrivas <harshitshrivas88@gmail.com>
2026-05-20 21:26:07 +0530

    Reverse Vowels of a String

---
+++ SlidingWindow/reversevowel.java
@@ -0,0 +1,34 @@
+public class reversevowel {
+    public static String  reverseVowels(String s){
+     char [] ch= s.toCharArray();
+     int left = 0;
+     int right = s.length() -1;
+     while(left < right){
+        if(!isVowel(ch[left])){
+            left ++;
+        }else if(!isVowel(ch[right])){
+            right --;
+        }
+        else {
+            char temp = ch[left];
+            ch[left] =ch[right];
+            ch[right] =temp;
+            left ++;
+            right --;
+        }
+     }
+     return new String(ch);
+    }
+
+    public static boolean isVowel(char c){
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
+               c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
+      
+    }
+    public static void main(String[] args) {
+        String s = "IceCreAm";
+        String result = reverseVowels(s);
+        System.out.println("Original string: " + s);
+        System.out.println("String after reversing vowels: " + result);
+    }
+}
