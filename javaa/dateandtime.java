0b8a59a8d9367ed6c8fd98b615f9a97cfb52de5d
Harshit Shrivas <harshitshrivas88@gmail.com>
2026-06-03 20:56:20 +0530

    Date and time

--- javaa/dateandtime.java
+++ javaa/dateandtime.java
@@ -3,12 +3,12 @@ import java.time.LocalDateTime;
 
 public class dateandtime {
     public static void main(String[] args) {
-        
+
         LocalDateTime now = LocalDateTime.now();
         System.out.println("Current date and time: " + now);
         LocalDateTime specificDateTime = LocalDateTime.of(2022, 12, 25, 10, 30);
         System.out.println("Specific date and time: " + specificDateTime);
-        LocalDate day = LocalDate.now();
+        LocalDate day = LocalDate.now(); // Get the current date
         System.out.println("Current date: " + day.plusDays(10));
 
     }
