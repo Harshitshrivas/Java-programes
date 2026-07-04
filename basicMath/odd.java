441a911b0f65e62014571b030f4e30f63d59e3f8
Harshit Shrivas <harshitshrivas88@gmail.com>
2026-07-04 16:51:15 +0530

    Odd even

--- basicMath/odd.java
+++ basicMath/odd.java
@@ -9,13 +9,21 @@ public class odd {
         int num = sc.nextInt();
 
         System.out.println("Odd and even numbers from 1 to " + num + " are: ");
-        
+
         for (int i = 1; i <= num; i++) { // ek sath odd aur even dono print karne ke liye
             if (i % 2 == 0) {
                 System.out.println("Even " + i);
             } else {
                 System.out.println("Odd " + i);
             }
+        }
+
+        // optimal aproch for odd and even number check
+        System.out.println("Enter a number to check odd or even: ");
+        if((num & 1) == 0){
+            System.out.println(num + " is Even...");
+        }else{
+            System.out.println(num + " is Odd..");
         }
 
     }
