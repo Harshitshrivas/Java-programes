aec5e9e4bb2dfb4163f8ac4d023beb8a8cf38641
Harshit Shrivas <harshitshrivas88@gmail.com>
2026-07-07 00:15:38 +0530

    Happy Number

--- basicMath/Happynum.java
+++ basicMath/Happynum.java
@@ -1,6 +1,31 @@
+import java.util.HashSet;
+
 public class Happynum {
+
+    public static int square(int num){
+
+        int ans = 0;
+        while(num > 0){
+            int remainder = num % 10;
+            ans += remainder * remainder;
+            num/=10;
+        }
+        return ans;
+    }
+
+    public static boolean isHappy(int num){
+
+        HashSet<Integer> set = new HashSet<>();
+
+        while(num != 0 && !set.contains(num)){
+            set.add(num);
+            num = square(num);
+        }
+        return  num==1;
+    }
+
     public static void main(String[] args) {
         int num = 19;
-        
+        System.out.println(isHappy(num));
     }
 }
