e9e56de3e3b500052125a39816b16020701c93f8
Harshit Shrivas <harshitshrivas88@gmail.com>
2026-08-20 00:06:54 +0530

    Single and Multiple non-repeating elements

--- prac/nonrepative.java
+++ prac/nonrepative.java
@@ -1,3 +1,6 @@
+
+import java.util.HashMap;
+
 public class nonrepative {
 
     static int nonnduplicate(int arr[]){
@@ -11,6 +14,23 @@ public class nonrepative {
     public static void main(String[] args) {
       int arr[] = { 1, 2, 3, 4, 1, 3, 4};
       System.out.println(nonnduplicate(arr)); 
-      
+
+      int arr2[] = { 1, 2, 3, 4, 1, 4}; // Multiple Non repative nikalne
+      HashMap<Integer , Integer> map = new HashMap<>();
+
+        // Frequency count
+      for(int i =0; i<arr2.length; i++){
+        map.put(arr2[i], map.getOrDefault(arr2[i], 0)+1);
+      }
+
+        // Print elements having frequency 1
+       System.out.print("Non-repeating elements: ");
+
+      for(int j=0; j<arr2.length; j++){
+        if(map.get(arr2[j]) == 1){
+            System.out.print(arr2[j] + " ");
+        }
+      }
+    
     }
 }
