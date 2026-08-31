2ca89001691dc3ec4dffb2b493facd592d6d7fb2
Harshit Shrivas <harshitshrivas88@gmail.com>
2026-08-31 14:24:14 +0530

    Diameter of Binary Tree

---
+++ treee/diametertree.java
@@ -0,0 +1,47 @@
+public class diametertree {
+
+    static class Node {
+
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static int diameterTree(Node node) {
+        if (node == null) {
+            return 0;
+        }
+        int left = diameterTree(node.left);
+        int right = diameterTree(node.right);
+        int diameter = heightTree(node.left) + heightTree(node.right);
+        return Math.max(diameter, Math.max(left, right));
+    }
+
+    static int heightTree(Node node) {
+        if (node == null)
+            return 0;
+
+        int left = heightTree(node.left);
+        int right = heightTree(node.right);
+
+        return Math.max(left, right) + 1;
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        System.out.println("Diameter of the binary tree is: " + diameterTree(root));
+    }
+}
