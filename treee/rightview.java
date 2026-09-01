32784dbcbb115a49c881915a93d641f5bdc8cec2
Harshit Shrivas <harshitshrivas88@gmail.com>
2026-09-02 00:57:32 +0530

    Binary Tree Right Side View

---
+++ treee/rightview.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class rightview {
+    public static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static List<Integer> rightSideView(Node root) { 
+        
+        Map<Integer, Integer> ans = new HashMap<>();
+        rightView(root, ans, 0);
+        return new ArrayList<>(ans.values());
+    }
+
+    static void rightView(Node root, Map<Integer, Integer> ans, int level) {
+        if (root == null) {
+            return;
+        }
+        ans.put(level, root.data);
+        rightView(root.left, ans, level + 1);
+        rightView(root.right, ans, level + 1);
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+        System.out.println(rightSideView(root));
+    }
+}
