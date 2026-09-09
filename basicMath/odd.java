049767ddb6ceb88ca00b7c00414b1ba1ae04b32d
Harshit Shrivas <harshitshrivas88@gmail.com>
2026-09-09 16:46:27 +0530

    Odd and even

--- basicMath/odd.java
+++ basicMath/odd.java
@@ -4,26 +4,27 @@ import java.util.Scanner;
 public class odd {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a number: ");
-        int num = sc.nextInt();
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter a number: ");
+            int num = sc.nextInt();
 
-        System.out.println("Odd and even numbers from 1 to " + num + " are: ");
+            System.out.println("Odd and even numbers from 1 to " + num + " are: ");
 
-        for (int i = 1; i <= num; i++) { // ek sath odd aur even dono print karne ke liye
-            if (i % 2 == 0) {
-                System.out.println("Even " + i);
-            } else {
-                System.out.println("Odd " + i);
+            for (int i = 1; i <= num; i++) { // ek sath odd aur even dono print karne ke liye
+                if (i % 2 == 0) {
+                    System.out.println("Even " + i);
+                } else {
+                    System.out.println("Odd " + i);
+                }
             }
-        }
 
-        // optimal aproch for odd and even number check
-        System.out.println("Enter a number to check odd or even: ");
-        if((num & 1) == 0){
-            System.out.println(num + " is Even...");
-        }else{
-            System.out.println(num + " is Odd..");
+            // optimal aproch for odd and even number check
+            System.out.println("Enter a number to check odd or even: ");
+            if((num & 1) == 0){
+                System.out.println(num + " is Even...");
+            }else{
+                System.out.println(num + " is Odd..");
+            }
         }
 
     }
